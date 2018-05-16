f2138d0b52891a0432a6cf0c78449a76809ddd78
JM De Ocampo <jmdeocampo@outlook.com>
2018-05-15 17:52:21 -0700

    Added unit tests

---
+++ Ch05_Graphs/Ch05_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ch05_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ch05_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0303158-a7ea-474d-9dc2-a24e9f43def7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ch05_Graphs/Ch05_UnitTests/UT_Q_02_TopSort_01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch05
+{
+    using D1 = ADS_01_Graph;
+    using Q02 = AQ_02_TopologicalSort_01<string>;
+
+    [TestClass]
+    public class UT_Q_02_TopSort_01
+    {
+        D1.Graph<string> g1 = new D1.Graph<string>();
+
+        [TestMethod]
+        public void Ch05_AQ_02_ReturnCorrectResult()
+        {     
+            //create graph
+            D1.AutoCreateGraph_03_TopSort_Directed_for_AQ_02_string(g1);
+
+            //Get result in a string no spaces
+            List<D1.Node<string>> resultList = Q02.TopSort(g1);
+            StringBuilder sb = new StringBuilder();
+
+            foreach(D1.Node<string> node in resultList)
+            {
+                sb.Append(node.Id);
+            }
+
+            Assert.AreEqual(sb.ToString(), "ABDECFHGIJ");
+        }
+    }
+}
